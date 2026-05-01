ca9762e3828bcd57f3c75de95729d7c31d564bc2
IvanYatsenko <yatsenko.ivan@student.sumdu.edu.ua>
2026-05-01 22:25:23 +0300

    Add Intern class extending Employee with university attribute

--- src/main/java/com/lab/Intern.java
+++ src/main/java/com/lab/Intern.java
@@ -1,4 +1,20 @@
 package com.lab;
 
-public class Intern {
+/**
+ * Клас, що представляє стажера.
+ * Успадковується напряму від базового класу Employee.
+ */
+public class Intern extends Employee {
+    private String universityName;
+
+    public Intern(String firstName, String lastName, double salary, String universityName) {
+        super(firstName, lastName, salary); // Виклик конструктора Employee
+        this.universityName = universityName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[Стажер] %s %s, Стипендія: %.2f, Університет: %s",
+                getFirstName(), getLastName(), getSalary(), universityName);
+    }
 }
