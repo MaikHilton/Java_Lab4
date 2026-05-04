01cdf1a5a8dc290896a9015d8cdb00cdb067f7f3
IvanYatsenko <yatsenko.ivan@student.sumdu.edu.ua>
2026-05-04 10:53:04 +0300

    Add Intern class with university details

--- src/main/java/com/lab/Intern.java
+++ src/main/java/com/lab/Intern.java
@@ -2,19 +2,33 @@ package com.lab;
 
 /**
  * Клас, що представляє стажера.
- * Успадковується напряму від базового класу Employee.
+ * Успадковується від {@link Employee} та містить інформацію про навчальний заклад.
  */
 public class Intern extends Employee {
     private String universityName;
 
+    /**
+     * Конструктор для стажера.
+     *
+     * @param firstName      Ім'я
+     * @param lastName       Прізвище
+     * @param salary         Стипендія або ставка стажера
+     * @param universityName Назва університету, де навчається стажер
+     */
     public Intern(String firstName, String lastName, double salary, String universityName) {
-        super(firstName, lastName, salary); // Виклик конструктора Employee
+        super(firstName, lastName, salary);
+        setType("Intern");
         this.universityName = universityName;
     }
 
     @Override
     public String toString() {
-        return String.format("[Стажер] %s %s, Стипендія: %.2f, Університет: %s",
+        return String.format("[Стажер] %s %s, Стипендія: %.2f, ВНЗ: %s",
                 getFirstName(), getLastName(), getSalary(), universityName);
+    }
+
+    @Override
+    public String toFileString() {
+        return "Intern;" + getFirstName() + ";" + getLastName() + ";" + getSalary() + ";" + universityName;
     }
 }
