3782b03dc94902b804e3bdbb0412e410a515be9e
IvanYatsenko <yatsenko.ivan@student.sumdu.edu.ua>
2026-05-04 10:52:52 +0300

    Add ContractEmployee class with contract duration

--- src/main/java/com/lab/ContractEmployee.java
+++ src/main/java/com/lab/ContractEmployee.java
@@ -2,32 +2,33 @@ package com.lab;
 
 /**
  * Клас, що представляє контрактного працівника.
- * Містить інформацію про термін дії контракту.
+ * Успадковується від {@link Employee} та містить інформацію про тривалість контракту.
  */
 public class ContractEmployee extends Employee {
     private int contractMonths;
 
     /**
      * Конструктор для контрактного працівника.
-     * @param firstName Ім'я
-     * @param lastName Прізвище
-     * @param salary Ставка за місяць
-     * @param contractMonths Термін контракту в місяцях
+     *
+     * @param firstName      Ім'я
+     * @param lastName       Прізвище
+     * @param salary         Ставка за місяць
+     * @param contractMonths Термін дії контракту (у місяцях)
      */
     public ContractEmployee(String firstName, String lastName, double salary, int contractMonths) {
         super(firstName, lastName, salary);
+        setType("ContractEmployee");
         this.contractMonths = contractMonths;
     }
 
-    /**
-     * Перевизначений метод для виведення інформації про контракт.
-     * @return Рядок з деталями контракту
-     */
     @Override
     public String toString() {
         return String.format("[Контрактник] %s %s, Ставка: %.2f, Термін: %d міс.",
                 getFirstName(), getLastName(), getSalary(), contractMonths);
     }
+
+    @Override
+    public String toFileString() {
+        return "ContractEmployee;" + getFirstName() + ";" + getLastName() + ";" + getSalary() + ";" + contractMonths;
+    }
 }
-
-
